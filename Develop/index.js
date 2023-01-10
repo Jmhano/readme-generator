7d9997ec86f06d8490a0e189f66f434141203884
Jmhano <jmhano@gmail.com>
2023-01-10 16:36:08 -0500

    created question array

--- Develop/index.js
+++ Develop/index.js
@@ -1,25 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
+const pageReadme = generateMarkdown(title, purpose, technology, developer);
 const [title, purpose, technology, developer] = profileDataArgs;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project title?",
+    },
+    {
+      type: "input",
+      name: "purpose",
+      message: "What is your project purpose?",
+    },
+    {
+      type: "checkbox",
+      name: "technology",
+      message: "What technologies did you use?:",
+      choices: [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node",
+      ],
+    },
+    {
+      type: "input",
+      name: "developer",
+      message: "Who worked on developing this project?:",
+    },
+    {
+      type: "input",
+      name: "link",
+      message: "Enter the GitHub link to your project. (Required)",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
-fs.writeFile(
-  "README.md",
-  generateMarkdown(title, purpose, technology, developer),
-  (err) => {
-    if (err) throw err;
+fs.writeFile("README.md", pageReadme, (err) => {
+  if (err) throw err;
 
-    console.log("Readme complete! Check out README.md to see the output!");
-  }
-);
+  console.log("Readme complete! Check out README.md to see the output!");
+});
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
-init();
+promptUser()
+  .then(answers => console.log(answers))
